7f6b11951970e3a7678376a7aff314350a5188a1
allamsubhash <allamsubhashchowdary@gmail.com>
2025-10-22 19:27:23 +0530

    Add files via upload

---
+++ artifact-demo/src/main/java/com/devops/App.java
@@ -0,0 +1,7 @@
+package com.devops;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello from Jenkins CI/CD with Nexus Artifact Management!");
+    }
+}
